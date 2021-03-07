461e65a3e88a14075934f66a82cfc968e2c5bdf0
Manuel Achenapure <machenapure@gmail.com>
2021-03-06 21:01:03 -0300

    Challenge Deliverable - Class 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return <>
+        <h1>Pacha Sustentable</h1>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </>
+}
+
+export default NavBar;
